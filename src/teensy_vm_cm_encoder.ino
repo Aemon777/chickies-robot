bcd8b17986702a8e22a623ed207ca6d8069e238c
joshmundell <57550021+joshmundell@users.noreply.github.com>
2021-10-21 15:49:02 -0400

    This is a little better
    I just rearranged it to function in a more reasonable way

--- src/teensy_vm_cm_encoder.ino
+++ src/teensy_vm_cm_encoder.ino
@@ -5,12 +5,13 @@
 Encoder knobLeft(5, 6);
 Encoder knobRight(7, 8);
 
-unsigned long previousMillis = 0;
+static unsigned long previousPow = 0;
+static unsigned long previousEnc = 0;
 double positionLeft  = 0;
 double positionRight = 0;
 
 //1 second
-const long interval = 1000;
+const long second = 1000;
 
 Adafruit_ADS1115 ads;
 void setup() {
@@ -20,12 +21,14 @@ void setup() {
 
 void loop() {
   unsigned long current = millis();
-  static unsigned long previous = 0;
-  if(current - previous > 100) {
-    previous = current;
+  if(current - previousPow > 100) {
+    previousPow = current;
     measurePower();
   }
-  trackEncoders();
+  if(current - previousEnc > second){
+    previousEnc = current;
+    trackEncoders();
+  }
 }
 void setupADC() {
   Wire.begin(); // join i2c bus (address optional for master)
@@ -64,32 +67,26 @@ void measurePower() {
   Serial.println(data);
 }
 
-void trackEncoders(){
-  //keep track of milliseconds
-  unsigned long currentMillis = millis();
-  
-  if(currentMillis - previousMillis >= interval){
-    previousMillis = currentMillis;
-    double newLeft, newRight;
-    //divide by 215 for converting to centimeters
-    newLeft = knobLeft.read() / 215.00;
-    newRight = knobRight.read() / 215.00;
-    if (newLeft != positionLeft || newRight != positionRight) {
-      Serial.print("Left = ");
-      Serial.print(newLeft);
-      Serial.print(", Right = ");
-      Serial.print(newRight);
-      Serial.println();
-      positionLeft = newLeft;
-      positionRight = newRight;
-    }
-    // if a character is sent from the serial monitor,
-    // reset both back to zero.
-    if (Serial.available()) {
-      Serial.read();
-      Serial.println("Reset both knobs to zero");
-      knobLeft.write(0);
-      knobRight.write(0);
-    }
+void trackEncoders(){  
+  double newLeft, newRight;
+  //divide by 215 for converting to centimeters
+  newLeft = knobLeft.read() / 215.00;
+  newRight = knobRight.read() / 215.00;
+  if (newLeft != positionLeft || newRight != positionRight) {
+    Serial.print("Left = ");
+    Serial.print(newLeft);
+    Serial.print(", Right = ");
+    Serial.print(newRight);
+    Serial.println();
+    positionLeft = newLeft;
+    positionRight = newRight;
+  }
+  // if a character is sent from the serial monitor,
+  // reset both back to zero.
+  if (Serial.available()) {
+    Serial.read();
+    Serial.println("Reset both knobs to zero");
+    knobLeft.write(0);
+    knobRight.write(0);
   }
 }
