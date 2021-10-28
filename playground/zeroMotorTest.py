b4a54a9b28631c9f764c70a88a96bfbb71a29de5
Drew Murphy <amurphy141@cedarville.edu>
2021-10-28 19:41:21 +0000

    changed zeroMotorTest

--- playground/zeroMotorTest.py
+++ playground/zeroMotorTest.py
@@ -1,14 +1,15 @@
 import gpiozero as gpio
 from time import sleep
-
+print("past imports")
 leftPwm = gpio.PWMLED(13)
 leftDir = gpio.LED(6)
 rightPwm = gpio.PWMLED(12)
 rightDir = gpio.LED(5)
 
-
+print("declared vars")
 leftDir.on()
 rightDir.on()
+print("turned on wheels")
 for duty in range(51):
     leftPwm.value = duty
     sleep(0.05)
@@ -26,7 +27,7 @@ for duty in range(75,-1,-1):
     rightPwm.value = duty
     sleep(0.05)
 sleep(0.5)
-
+print("switching directions")
 leftDir.off()
 rightDir.off()
 for duty in range(51):
