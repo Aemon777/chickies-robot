3d2b159deb20479bdb6fb38ba5a4dc349384900e
Andrew Murphy <amurphy141@cedarville.edu>
2021-10-30 15:57:05 -0400

    Increasing max duty

--- playground/zeroMotorTest.py
+++ playground/zeroMotorTest.py
@@ -10,24 +10,24 @@ print("declared vars")
 leftDir.on()
 rightDir.on()
 print("turned on wheels")
-for duty in range(0, 51, 5):
+for duty in range(0, 76, 5):
     print(duty)
     leftPwm.value = duty/100
     sleep(0.2)
 print("loop 1")
 sleep(0.5)
-for duty in range(51,-1,-5):
+for duty in range(76,-1,-5):
     leftPwm.value = duty/100
     sleep(0.2)
 print("loop 2")
 sleep(0.5)
     
-for duty in range(0,51,5):
+for duty in range(0,76,5):
     rightPwm.value = duty/100
     sleep(0.2)
 print("loop 3")
 sleep(0.5)
-for duty in range(51,-1,-5):
+for duty in range(76,-1,-5):
     rightPwm.value = duty/100
     sleep(0.2)
 print("loop 4")
@@ -35,23 +35,23 @@ sleep(0.5)
 print("switching directions")
 leftDir.off()
 rightDir.off()
-for duty in range(0, 51, 5):
+for duty in range(0, 76, 5):
     leftPwm.value = duty/100
     sleep(0.2)
 print("loop 5")
 sleep(0.5)
-for duty in range(51,-1,-5):
+for duty in range(76,-1,-5):
     leftPwm.value = duty/100
     sleep(0.2)
 print("loop 6")
 sleep(0.5)
 
-for duty in range(0,51,5):
+for duty in range(0,76,5):
     rightPwm.value = duty/100
     sleep(0.2)
 print("loop 7")
 sleep(0.5)
-for duty in range(51,-1,-5):
+for duty in range(76,-1,-5):
     rightPwm.value = duty/100
     sleep(0.2)
 print("loop 8")
