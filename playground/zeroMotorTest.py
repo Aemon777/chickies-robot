f9685fc7b6facc79cca3fcff320bf87717f341e4
Andrew Murphy <amurphy141@cedarville.edu>
2021-10-30 16:01:45 -0400

    Reset system to original test format

--- playground/zeroMotorTest.py
+++ playground/zeroMotorTest.py
@@ -10,50 +10,50 @@ print("declared vars")
 leftDir.on()
 rightDir.on()
 print("turned on wheels")
-for duty in range(0, 91, 5):
+for duty in range(0, 91, 1):
     print(duty)
     leftPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 1")
 sleep(0.5)
-for duty in range(91,-1,-5):
+for duty in range(91,-1,-1):
     leftPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 2")
 sleep(0.5)
     
-for duty in range(0,91,5):
+for duty in range(0,91,1):
     rightPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 3")
 sleep(0.5)
-for duty in range(91,-1,-5):
+for duty in range(91,-1,-1):
     rightPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 4")
 sleep(0.5)
 print("switching directions")
 leftDir.off()
 rightDir.off()
-for duty in range(0, 91, 5):
+for duty in range(0, 91, 1):
     leftPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 5")
 sleep(0.5)
-for duty in range(91,-1,-5):
+for duty in range(91,-1,-1):
     leftPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 6")
 sleep(0.5)
 
-for duty in range(0,91,5):
+for duty in range(0,91,1):
     rightPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 7")
 sleep(0.5)
-for duty in range(91,-1,-5):
+for duty in range(91,-1,-1):
     rightPwm.value = duty/100
-    sleep(0.2)
+    sleep(0.01)
 print("loop 8")
 sleep(0.5)
 sleep(0.5)
