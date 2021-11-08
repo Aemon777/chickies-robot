480fc9b1fbf99f7da49fec125a5f88fa3475ff0d
Andrew Murphy <amurphy141@cedarville.edu>
2021-11-08 15:34:24 -0500

    Adjusted motor test to test encoder speed

--- playground/motorTest.py
+++ playground/motorTest.py
@@ -1,59 +1,22 @@
-import RPi.GPIO as GPIO
+import gpiozero as gpio
 from time import sleep
+print("past imports")
 
-pwmA = 33
-dirA = 31
-pwmB = 32
-dirB = 29
-
-GPIO.setwarnings(False)			#disable warnings
-GPIO.setmode(GPIO.BOARD)		#set pin numbering system
-GPIO.setup(pwmA,GPIO.OUT)
-GPIO.setup(dirA,GPIO.OUT)
-GPIO.setup(pwmB,GPIO.OUT)
-GPIO.setup(dirB,GPIO.OUT)
-pwma = GPIO.PWM(pwmA,1000)		#create PWM instance with frequency
-pwmb = GPIO.PWM(pwmB,1000)		#create PWM instance with frequency
-pwma.start(0)				#start PWM of required Duty Cycle 
-pwmb.start(0)
+leftPwm = gpio.PWMLED(13)
+leftDir = gpio.LED(6)
+rightPwm = gpio.PWMLED(12)
+rightDir = gpio.LED(5)
 
 while True:
-    GPIO.output(dirA,True)
-    GPIO.output(dirB,True)
-    for duty in range(0,76,1):
-        pwma.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+    leftDir.on()
+    rightDir.on()
+    for duty in range(0,91,1):
+        leftPwm.value = duty/100 #provide duty cycle in the range 0-100
+        rightPwm.value = duty/100
         sleep(0.05)
-    sleep(0.5)
-    for duty in range(75,-1,-1):
-        pwma.ChangeDutyCycle(duty)
+    sleep(3)
+    for duty in range(91,-1,-1):
+        leftPwm.value = duty/100
+        rightPwm.value = duty/100
         sleep(0.05)
-    sleep(0.5)
     
-    for duty in range(0,76,1):
-        pwmb.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.05)
-    sleep(0.5)
-    for duty in range(75,-1,-1):
-        pwmb.ChangeDutyCycle(duty)
-        sleep(0.05)
-    sleep(0.5)
-
-    GPIO.output(dirA,False)
-    GPIO.output(dirB,False)
-    for duty in range(0,76,1):
-        pwma.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.05)
-    sleep(0.5)
-    for duty in range(75,-1,-1):
-        pwma.ChangeDutyCycle(duty)
-        sleep(0.05)
-    sleep(0.5)
-    
-    for duty in range(0,76,1):
-        pwmb.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.05)
-    sleep(0.5)
-    for duty in range(75,-1,-1):
-        pwmb.ChangeDutyCycle(duty)
-        sleep(0.05)
-    sleep(0.5)
