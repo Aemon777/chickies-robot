5bcf55dde8195c0cce73989574d1d18bf31021a6
Andrew Murphy <amurphy141@cedarville.edu>
2021-11-08 16:22:26 -0500

    Adjusted test names and added even

---
+++ playground/evenMotorTest.py
@@ -0,0 +1,21 @@
+import gpiozero as gpio
+from time import sleep
+
+leftPwm = gpio.PWMLED(13)
+leftDir = gpio.LED(6)
+rightPwm = gpio.PWMLED(12)
+rightDir = gpio.LED(5)
+
+leftDir.off()
+rightDir.off()
+for duty in range(0,100,1):
+    leftPwm.value = duty/100/1.125 #provide duty cycle in the range 0-100
+    sleep(0.01)
+    rightPwm.value = duty/100
+    sleep(0.01)
+sleep(3)
+for duty in range(100,-1,-1):
+    leftPwm.value = duty/100/1.125
+    sleep(0.01)
+    rightPwm.value = duty/100
+    sleep(0.01)
