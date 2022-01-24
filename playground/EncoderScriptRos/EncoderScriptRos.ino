bcc787abf69f4b2cfca40601c897852bd9803c24
Andrew Murphy <anmurphy00@gmail.com>
2022-01-24 16:46:03 -0500

    Commiting to view on other CPU

--- playground/EncoderScriptRos/EncoderScriptRos.ino
+++ playground/EncoderScriptRos/EncoderScriptRos.ino
@@ -1,51 +1,71 @@
 #include <ros.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Int16.h>
+#include <std_msgs/Int64.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int8.h>
 #include <Encoder.h>
 
 #define leftDirPin 30;
 #define leftPwmPin 28;
 #define rightDirPin 31;
 #define rightPwmPin 29;
+#define interval 1000;
 
 Encoder knobLeft(5, 6);
 Encoder knobRight(7, 8);
 
+unsigned long previousMillis = 0;
+double positionLeft = 0;
+double positionRight = 0;
+
 ros::NodeHandle nodeHandle;
 
-std_msgs::String encoderMsg;
-std_msgs::Int16 leftWheelMsg;
-std_msgs::Int16 rightWheelMsg;
+std_msgs::Int64 leftTicskMsg;
+std_msgs::Int64 rightTicskMsg;
+std_msgs::Float32 leftVelMsg;
+std_msgs::Float32 rightVelMsg;
+std_msgs::Int8 encoderResetMsg;
 
-void leftCallback(const std_msgs::Int16& leftWheelMsg) {
-  int16_t leftPwm = leftWheelMsg.data;
-  if (leftPwm < 0) {
+void leftCallback(const std_msgs::Int16& leftVelMsg) {
+  int16_t vel = leftVelMsg.data;
+  if (vel < 0) {
     digitalWrite(leftDirPin, LOW);
   }
   else {
     digitalWrite(leftDirPin, HIGH);
   }
-  leftMag = (int) abs(leftPwm)*255/100;
+  pwm = ??
   
-  analogWrite(leftPwmPin, leftMag);
+  analogWrite(leftPwmPin, pwm);
 }
 
-void rightCallback(const std_msgs::Int16& rightWheelMsg) {
-  int16_t rightPwm = rightWheelMsg.data;
-  if (rightPwm < 0) {
+void rightCallback(const std_msgs::Int16& rightVelMsg) {
+  int16_t vel = rightVelMsg.data;
+  if (vel < 0) {
     digitalWrite(rightDirPin, LOW);
   }
   else {
     digitalWrite(rightDirPin, HIGH);
   }
-  rightMag = (int) abs(rightPwm)*255/100;
+  pwm = ??
   
-  analogWrite(rightPwmPin, rightMag);
+  analogWrite(rightPwmPin, pwm);
 }
+
+ros::Publisher leftTickPublisher("/Odometry/leftTicks", &leftTicksMessage);
+ros::Publisher rightTickPublisher("/Odometry/rightTicks", &encoderMsg);
+ros::Subscriber<std_msgs::Int16> leftSubscriber("leftVelocity", &leftVelMsg);
+ros::Subscriber<std_msgs::Int16> rightSubscriber("rightVelocity", &rightVelMsg);
   
 void setup() {
-  //
+  pinMode(leftDirPin, OUTPUT);
+  pinMode(leftPwmPin, OUTPUT);
+  pinMode(rightDirPin, OUTPUT);
+  pinMode(rightPwmPin, OUTPUT);
 
+  nodeHandle.initNode();
+  nodeHandle.advertise(encoder_publisher);
+  nodeHandle.subscribe(left_subscriber);
+  nodeHandle.subscribe(right_subscriber);
 }
 
 void loop() {
