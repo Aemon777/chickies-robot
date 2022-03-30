dc097e49e720402c35a8a854ddee13d2b921edde
Drew Murphy <amurphy141@cedarville.edu>
2022-03-30 14:20:44 -0400

    some testing

--- catkin_ws/src/motor_controller/scripts/radio_receiver.py
+++ catkin_ws/src/motor_controller/scripts/radio_receiver.py
@@ -37,7 +37,7 @@ class radio_receiver():
             for_rev = (int(data[4]) < 50)
             throttle = int(data[2])
             steer = -1 if int(data[0]) == 0 else int(data[0])
-            steerLeft = (steer)*1.5 - 75
+            steerLeft = -1*(steer)*1.5 + 75
             vel = Twist()
             vel.linear.x = throttle*1.25/200
             if not for_rev:
